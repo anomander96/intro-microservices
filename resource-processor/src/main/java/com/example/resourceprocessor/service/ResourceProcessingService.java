450808477ff9b7c28050cacd61031bf23f9c3bf7
anomander96 <mister.truepaz@gmail.com>
2024-01-23 19:01:27 +0200

    fixed issue with file size

--- resource-processor/src/main/java/com/example/resourceprocessor/service/ResourceProcessingService.java
+++ resource-processor/src/main/java/com/example/resourceprocessor/service/ResourceProcessingService.java
@@ -1,12 +1,12 @@
 package com.example.resourceprocessor.service;
 
 import com.example.resourceprocessor.dto.ResourceDTO;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.Recover;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Slf4j
@@ -17,13 +17,23 @@ public class ResourceProcessingService {
     private final WebClient songServiceWebClient;
 
     public ResourceProcessingService(WebClient.Builder webClientBuilder){
+        ExchangeStrategies exchangeStrategies = ExchangeStrategies.builder()
+                .codecs(config -> config
+                        .defaultCodecs()
+                        .maxInMemorySize(100 * 1024 * 1024)) // 100 MB size is allowed without any Exceptions
+                .build();
+
         //Create a WebClient for the Resource Service
-        this.resourceServiceWebClient = webClientBuilder.baseUrl("http://resource-service").build();
+        this.resourceServiceWebClient = webClientBuilder.baseUrl("http://resource-service:8081/resources")
+                .exchangeStrategies(exchangeStrategies)
+                .build();
         //Create a WebClient for the Song Service
-        this.songServiceWebClient = webClientBuilder.baseUrl("http://song-service").build();
+        this.songServiceWebClient = webClientBuilder.baseUrl("http://song-service:8082")
+                .exchangeStrategies(exchangeStrategies)
+                .build();
     }
 
-//    @Retryable(maxAttempts = 5, backoff = @Backoff(delay = 2000, multiplier = 2))
+    @Retryable(maxAttempts = 5, backoff = @Backoff(delay = 2000, multiplier = 2))
     public void processResource(Integer resourceLocationId) {
         // Fetch the resource object from the Resource Service using a synchronous call
         ResourceDTO resourceDto = resourceServiceWebClient.get()
@@ -37,6 +47,8 @@ public class ResourceProcessingService {
             return;
         }
 
+        System.out.println(resourceDto.toString());
+
         // Send the resource to the Song Service using a synchronous call
         songServiceWebClient.post()
                 .uri("/songs")
@@ -49,6 +61,7 @@ public class ResourceProcessingService {
     @Recover
     public void recover(Exception e, Integer resourceId){
         // handle recovery logic here
+        log.info("Recovering started...");
     }
 
 }
